ac99f6a1294831ce5167623224552ee9ee6b508f
Ace3DF <Ace3DF@gmail.com>
2015-07-11 23:44:38 +0100

    Big Money

--- main.py
+++ main.py
@@ -1,99 +1,16 @@
 #-*- coding: utf-8 -*-
-from utils import get_series_info
-from utils import check_new_name
-from utils import replace_all
-from utils import scrape_site
 import feedparser
-#import requests # For another time
 import hashlib
 import cPickle
-#import bencode # For another time
 import urllib
 import base64
+import utils
 import time
 import re
 
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
-
-""" TODO:
-Re-add episode remove/add support.
-Sort out 1080p, 720p, 480p order for post
-"""
-
-def html_download_div(filename="filename", links=[]):
-	OtakuShare = ""
-	Torrent = ""
-	Magnet = ""
-	Go4UP = ""
-	Hugefiles = ""
-	Uploaded = ""
-	for link in links:
-		if "otakushare.com" in link:
-			OtakuShare = link
-		elif "go4up.com" in link:
-			Go4UP = link
-		elif "hugefiles.net" in link:
-			Hugefiles = link
-		elif "uploaded.net" in link:
-			Uploaded = link
-		elif "nyaa.se" in link:
-			Torrent = link
-		elif "magnet:" in link:
-			Magnet = link
-	html = r"""
-<div class="dl-box">
-	<div class="dl-title">{0}</div>
-	<div class="dl-item"><a href="{1}">OtakuShare</a> <strong><span style="color: #800000;">◀◀</span></strong> <strong><span style="color: #000000;">Recommended</span></strong></div>
-	<div class="dl-item"><a href="{2}">Torrent</a> <a href="{3}">(Magnet)</a> ◇ <a href="{4}">Go4UP</a> ◇ <a href="{5}">Hugefiles</a> ◇ <a href="{6}">Uploaded</a></div>
-</div>""".format(filename, OtakuShare, Torrent, Magnet, Go4UP, Hugefiles, Uploaded)
-	return html
-
-def re_episode_num(filename):
-	rep = {"480p": "", "720p": "", "1080p": "", \
-	"1280×720": "", "": "", "mkv": "", "mp4": "", "avi": ""}
-	filename = replace_all(filename, rep)
-	if "movie" in filename.lower():
-		return None
-	if "persona" in filename.lower():
-		filename = filename[10:]
-	filename = re.sub(r'\[(?:[^\]|]*\|)?([^\]|]*)\]', '', filename).strip()
-	temp_name = replace_all(filename, rep)
-	match = re.search(
-		r'''(?ix)				 # Ignore case (i), and use verbose regex (x)
-		(?:					   # non-grouping pattern
-		  | - | – |x|episode|^		   # e or x or episode or start of a line
-		  )					   # end non-grouping pattern 
-		\s*					   # 0-or-more whitespaces
-		(\d{2}\.\d{1}|\d{1}\.\d{1}|\d{3}|\d{2}|\d{1})				   # exactly 2/3 digits
-		''', temp_name)
-	if match:
-		return match.group(1)
-
-def re_series_name(filename):
-	filename = re.sub(r'\[(?:[^\]|]*\|)?([^\]|]*)\]', '', filename).strip()
-	epinum = re_episode_num(filename)
-	if epinum is None:
-		# Movie / one off OVA/ONA
-		pass
-	else:
-		at = filename.index(epinum)
-		filename = filename[:at]
-	fullstops = filename.count('.')
-	if fullstops > 1:
-		filename = filename.replace(".", " ")
-	rep = {"mkv": "", "mp4": "", "avi": "", " –": ":", " -": ""}
-	filename = replace_all(filename, rep)
-	if filename[-2] == ":":
-		filename = filename[0:-2]
-	filename = re.sub(' +',' ', filename).strip()
-	old_filename = filename
-	title_names = {}
-	new = check_new_name(filename)
-	if new is not None:
-		filename = new
-	return filename
 
 def main():
 	while True:
@@ -110,18 +27,24 @@ def main():
 
 		rss_count = 0
 		for a in d:
+			if rss_count == 0:
+				rss_count += 1
+				continue
 			skip = False
 			summary_html = ""
-			filename = a.title
 			post_id = a.guid
 			html = ""
 			if post_id in already_used:
 				rss_count += 1
 				continue
+			video_rez = utils.html_decode(re.findall('Video: (.*?)\<br />', \
+				a.content[0]['value'])[0]).split(',')[2].split('×')[1].lstrip()
+			filename = utils.html_decode(re.findall('Release name: (.*?)\<br />', \
+				a.content[0]['value'])[0])
 			magnet_link = re.findall('(magnet:\?xt=[^\"<]*)', \
-				a.content[0]['value'].decode('utf-8'))
+				a.content[0]['value'])
 			download_urls = re.findall('<a href="?\'?([^"\'>]*)', \
-				a.content[0]['value'].decode('utf-8'))
+				a.content[0]['value'])
 			download_urls.append(magnet_link)
 			if "otakubot" in download_urls[0] or "zupimages" in download_urls[0]:
 				download_urls.pop(0)
@@ -140,86 +63,41 @@ def main():
 					download_urls[count] = url.replace("Torrent", "")
 					download_urls[count + 1] = get_magnet(download_urls[count])
 				count += 1
-			try:
-				episode_num = float(re_episode_num(filename))
-				if (episode_num).is_integer():
-					episode_num = int(episode_num)
-			except:
-				# Movie/OVA
-				pass
 
-			series_name = re_series_name(filename)
-			anime_info = get_series_info(series_name)
+			episode_number = utils.get_episode_number(filename)
+			series_name = utils.get_new_name(utils.get_series_name(filename, episode_number))
+			if series_name == "SKIP":
+				continue
+			episode_number = episode_number + utils.get_remove_ep(series_name)
 
-			if anime_info[1] == "TV":
-				post_title = series_name + " Episode " + str(episode_num)
+			if episode_number == utils.get_last_ep(series_name):
+				# Is last episode
+				post_title = "{0} Episode {1} Final".format(series_name, episode_number)
+			elif not episode_number:
+				# Is movie/ova
+				post_title = "{0}".format(series_name)
 			else:
-				post_title = series_name
+				# Is normal episode
+				post_title = "{0} Episode {1}".format(series_name, episode_number)
+			# CHANGE TO 1
+			if episode_number <= 1 or not episode_number:
+				# New series
+				if not utils.get_if_stored(series_name):
+					utils.get_series_info(series_name)
 
-			if str(episode_num) == "1":
-				summary_html = anime_info[2]
-			elif episode_num is None:
-				summary_html = anime_info[2]
+			html = utils.html_download_div(series_name, episode_number, video_rez, \
+					filename, download_urls)
 
-			if series_name == "IGNORE":
-				skip = True
-
-			if skip != True:
-				html = html_download_div(filename, download_urls)
-				rss_count += 1
-				try:
-					while str(series_name) == str(re_series_name(d[rss_count].title)) and int(episode_num) == int(re_episode_num(d[rss_count].title)):
-						next_post = d[rss_count]
-						filename = next_post.title
-						already_used.append(next_post.guid)
-						download_urls = re.findall('<a href="?\'?([^"\'>]*)', \
-								next_post.content[0]['value'].decode('utf-8'))
-						count = 0
-						for url in download_urls:
-							if "Go4UP" in url[20:]:
-								download_urls[count] = url.replace("Go4UP", "")
-							elif "Hugefiles" in url[20:]:
-								download_urls[count] = url.replace("Hugefiles", "")
-							elif "Uploaded" in url[20:]:
-								download_urls[count] = url.replace("Uploaded", "")
-							elif "Torrent" in url[20:]:
-								download_urls[count] = url.replace("Torrent", "")
-							count += 1
-						html += html_download_div(filename, download_urls)
-						rss_count += 1
-						time.sleep(5)
-				except:
-					# Feed end
-					pass
-
-				already_used.append(post_id)
-				print "New Post:"
-				print post_title
-				print "HTML:"
-				print summary_html + "<br />" + html
-				break
+			print "New Post:"
+			print post_title
+			print
+			print "HTML:"
+			print html
+			already_used.append(post_id)
+			break
 
 		cPickle.dump(already_used, open("used_links.pkl", 'w'))
-		time.sleep(20)
+		time.sleep(15)
 
 if __name__ == "__main__":
 	main()
-	# Test stuff under
-	"""
-	test = "[FFF] Classroom Crisis – 01 [C6D2C330].mkv"
-	try:
-		episode_num = float(re_episode_num(test))
-		if (episode_num).is_integer():
-			episode_num = int(episode_num)
-	except:
-		# Movie/OVA
-		pass
-	page_title = re_series_name(test)
-	anime_info = get_series_info(page_title)
-	if anime_info[1] == "TV":
-		post_title = page_title + " Episode " + str(episode_num)
-	else:
-		post_title = page_title
-	print post_title
-	quit()"""
-
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 #-*- coding: utf-8 -*-
 from xml.etree import cElementTree as ET
+from os.path import isfile, join
+from natsort import natsorted
 from bs4 import BeautifulSoup
 from datetime import date
+from os import listdir
 import unicodedata
 import HTMLParser
 import requests
@@ -9,6 +12,7 @@ import difflib
 import urllib2
 import json
 import re
+import os
 
 import sys
 reload(sys)
@@ -38,37 +42,140 @@ def scrape_site(url, xml=False):
 	else:
 		return BeautifulSoup(response)
 
-def check_new_name(old):
-	with open('anime_data.json') as data_file:    
-		current_json = json.load(data_file)
-	for name in current_json:
-		if name.lower().strip() == old.lower().strip():
-			# In json, has new name!
-			for new in current_json[name.lower().strip()].iteritems():
-				if new[0] == "new_name":
-					return new[1]
+def get_series_name(filename, episode_number=False):
+	rep = {".mkv": "", ".mp4": "", ".avi": ""}
+	filename = replace_all(filename, rep)
+	filename = re.sub(r'\[(?:[^\]|]*\|)?([^\]|]*)\]', '', filename).strip()
+	filename = re.sub(r' +', ' ', filename)
 
-	return
+	if not episode_number:
+		# Don't know EP (Movie/OVA?)
+		pass
+	else:
+		at = filename.index(str(episode_number))
+		if filename[at-1:at] == "0":
+			at = at - 1
+		filename = filename[:at].strip()
+		if filename.endswith("-"):
+			filename = filename[:-1].strip()
 
-def store_new_name(old, anime_info):
-	with open('anime_data.json') as data_file:    
-		current_json = json.load(data_file)
-	for name in current_json:
-		if name.lower().strip() == old.lower().strip():
-			# Already in json, no need to do anything.
-			return
-	template = {"new_name": anime_info[0],
-				"image": anime_info[1],
-				"summary": anime_info[2],
-				"type": anime_info[3],
-				"total_eps": anime_info[4],
-				"id": anime_info[5],
-				"source": anime_info[6],
-				"remove_eps": "0"}
-	current_json[old.lower().strip()] = (template)
-	anime_data_file = open('anime_data.json', 'w')
-	json.dump(current_json, anime_data_file, indent=4)
-	anime_data_file.close()
+	# Clean filenames that replaces spaces with "."
+	fullstops = filename.count('.')
+	if fullstops > 1:
+		filename = filename.replace(".", " ").lstrip()
+
+	# Clean filenames that replaces spaces with "_"
+	fullstops = filename.count('_')
+	if fullstops > 1:
+		filename = filename.replace("_", " ").lstrip()
+	return filename.strip()
+
+def get_episode_number(filename):
+	filename = re.sub(r'\[(?:[^\]|]*\|)?([^\]|]*)\]', '', filename).strip()
+	match = re.search(r'\d{4}', filename)
+	if match:
+		if int(match.group()) < 2025 or int(match.group()) > 2010:
+			filename = filename.replace(match.group(), "")
+	match = re.search(
+		r'''(?ix)				 # Ignore case (i), and use verbose regex (x)
+		(?:					   # non-grouping pattern
+		  | - | – |x|episode|^		   # e or x or episode or start of a line
+		  )					   # end non-grouping pattern 
+		\s*					   # 0-or-more whitespaces
+		(\d{2}\.\d{1}|\d{1}\.\d{1}|\d{3}|\d{2})				   # exactly 2/3 digits
+		''', filename)
+	if match:
+		episode_num = float(match.group(1))
+		if (episode_num).is_integer():
+			episode_num = int(episode_num)
+		return episode_num
+	else:
+		# Couldn't get EP at all (Movie/OVA?)
+		return False
+
+def get_new_name(old):
+	show_list = open('anime_data.txt').read().splitlines()
+	for line in show_list:
+		line = line.split("||")
+		if old in line[0]:
+			return line[1]
+	return old
+
+def get_if_stored(name):
+	show_list = open('anime_data.txt').read().splitlines()
+	for line in show_list:
+		line = line.split("||")
+		if name == line[1]:
+			return True
+	return False
+
+def get_last_ep(name):
+	show_list = open('anime_data.txt').read().splitlines()
+	for line in show_list:
+		line = line.split("||")
+		if name in line[1]:
+			return int(line[2])
+	return 999
+
+def get_remove_ep(name):
+	show_list = open('anime_data.txt').read().splitlines()
+	for line in show_list:
+		line = line.split("||")
+		if name in line[1]:
+			return int(line[3])
+	return 0
+
+def get_subgroup(filename):
+	if "[" in filename[0:5]:
+		try:
+			m = re.search(r"\[([A-Za-z0-9_]+)\]", filename)
+			return m.group(1)
+		except:
+			return "NONE"
+	else:
+		return "NONE"
+
+def store_new_name(old, new, max, math):
+	show_list = open('anime_data.txt').read().splitlines()
+	for line in show_list:
+		line = line.split("||")
+		if old in line[1]:
+			return True
+	new_line = "{0}||{1}||{2}||{3}\n".format(old, new, max, math)
+	with open('anime_data.txt', 'a') as show_list:
+		show_list.write(new_line)
+
+	return True
+
+def store_html(series_name, episode_number, video_rez, group, html):
+	onlyfiles = [ f for f in listdir("html") if isfile(join("html",f)) ]
+	search_name = "{0} - {1} [".format(series_name, episode_number)
+	found_files = []
+	for html_file in onlyfiles:
+		if html_file.startswith(search_name):
+			found_files.append(html_file)
+	found_files = list(reversed(natsorted(found_files)))
+	filename = "{0} - {1} [{2}] [{3}].html".format(series_name, episode_number, video_rez, group)
+	if filename not in found_files:
+		path = os.path.join("html", filename)
+		found_files.append(filename)
+		with open(path, 'w') as openfile:
+			openfile.write(html)
+
+	html = ""
+	if episode_number <= 1 or not episode_number:
+		try:
+			info_file = "{0} - INFO.html".format(series_name)
+			path = os.path.join("html", info_file)
+			html += open(path, 'r').read()
+		except:
+			# No info from mal, etc.
+			pass
+
+	for html_file in found_files:
+		path = os.path.join("html", html_file)
+		html += open(path, 'r').read()
+	return html
 
 def html_decode(s):
 	"""
@@ -78,17 +185,19 @@ def html_decode(s):
 	"""
 	htmlCodes = (
 				("'", '&#39;'),
-				("’", '&amp;rsquo;'),
 				('"', '&quot;'),
 				('>', '&gt;'),
 				('<', '&lt;'),
 				('&', '&amp;'),
-				('and', '&'),
 				('—', '&mdash;'),
+				('-', '&#8211;'),
+				('×', '&#215;'),
+				('’', '&#8217;'),
 				('<i>', '[i]'),
 				('</i>', '[/i]'),
 				('<strong>', '[b]'),
 				('uu', 'ū'),
+				('and', '&'),
 				('</strong>', '[/b]')	
 			)
 	for code in htmlCodes:
@@ -96,7 +205,7 @@ def html_decode(s):
 	s = ''.join((c for c in unicodedata.normalize('NFD', unicode(s)) if unicodedata.category(c) != 'Mn')).encode('utf-8')
 	return s
 
-def html_download_div(filename="filename", links=[]):
+def html_download_div(ser, ep, rez, filename="filename", links=[]):
 	OtakuShare = ""
 	Torrent = ""
 	Magnet = ""
@@ -122,9 +231,10 @@ def html_download_div(filename="filename", links=[]):
 	<div class="dl-item"><a href="{1}">OtakuShare</a> <strong><span style="color: #800000;">◀◀</span></strong> <strong><span style="color: #000000;">Recommended</span></strong></div>
 	<div class="dl-item"><a href="{2}">Torrent</a> <a href="{3}">(Magnet)</a> ◇ <a href="{4}">Go4UP</a> ◇ <a href="{5}">Hugefiles</a> ◇ <a href="{6}">Uploaded</a></div>
 </div>""".format(filename, OtakuShare, Torrent, Magnet, Go4UP, Hugefiles, Uploaded)
+	html = store_html(ser, ep, rez, get_subgroup(filename), html)
 	return html
 
-def html_info(anime_info, site):
+def html_info(series_name, anime_info, site):
 	"""
 	anime_info:-
 	[0] = Series Name 
@@ -135,9 +245,14 @@ def html_info(anime_info, site):
 	[5] = Site Series ID
 	[6] = Site Used
 	"""
+	anime_info[2] = re.sub(r'\(Source:[^)]*\)', '', anime_info[2])
+	if anime_info[2].endswith("<br />\n"):
+		anime_info[2] = anime_info[2][:-7]
+	if anime_info[2].endswith("<br />\n"):
+		anime_info[2] = anime_info[2][:-7]
 	html_0 = "<p style=\"text-align: center;\"><img src=\"%s\" width=\"225px\" height=\"318px\" /></p>"  % anime_info[1]
 	html_1 = "<h3 style=\"text-align: center;\"><strong>Plot Summary</strong></h3>"
-	html_2 = "<p style=\"text-align: center;\">" + anime_info[2]
+	html_2 = "<p style=\"text-align: center;\">" + html_decode(anime_info[2]) + "</p>"
 	if site == "MAL":
 		mal_url = "http://myanimelist.net/anime/" + anime_info[5]
 	else:
@@ -147,9 +262,14 @@ def html_info(anime_info, site):
 	else:
 		ann_url = "http://www.animenewsnetwork.com/encyclopedia/search/name?q=" + anime_info[0].replace(" ", "+")
 	ap_url = "http://www.anime-planet.com/anime/all?name=" + anime_info[0].replace(" ", "+")
-	html_3 = "<br /><a href=\"%s\">MAL</a> | <a href=\"%s\">ANN</a> | <a href=\"%s\">AP</a>" % \
+	html_3 = "<p style=\"text-align: center;\"><a href=\"%s\">MAL</a> | <a href=\"%s\">ANN</a> | <a href=\"%s\">AP</a></p>" % \
 			(mal_url, ann_url, ap_url)
-	return html_0 + html_1 + html_2 + html_3
+	html = html_0 + html_1 + html_2 + html_3
+	filename = "{0} - INFO.html".format(series_name)
+	path = os.path.join("html", filename)
+	with open(path, 'w') as openfile:
+		openfile.write(html)
+	return html
 
 def MyAnimeList_search(query):
 	if query == "":
@@ -165,8 +285,7 @@ def MyAnimeList_search(query):
 	)
 	if (r.status_code == 204):
 		return []
-	xml_string = html_decode(r.text)
-	elements = ET.fromstring(xml_string)
+	elements = ET.fromstring(r.text)
 	results = [dict((attr.tag, attr.text) for attr in el) for el in elements]
 	full_names = []
 	full_list = []
@@ -174,7 +293,7 @@ def MyAnimeList_search(query):
 		if result['synopsis'] is None:
 			result['synopsis'] = "No synopsis."
 		if result['episodes'] == "0":
-			result['episodes'] = "??"
+			result['episodes'] = "999"
 		full_names.append(result['title'].lower())
 		full_list.append([result['title'], result['image'], html_decode(result['synopsis']), result['type'], result['episodes'], result['id'], "MAL"])
 	series_name = difflib.get_close_matches(query.lower(), full_names, 1, 0.25)
@@ -187,9 +306,8 @@ def MyAnimeList_search(query):
 			if series_name.lower().strip() == anime[0].lower().strip():
 				anime_info = anime
 				break
-	#if series_name.lower().strip() != orginal.lower().strip():
-	store_new_name(orginal, anime_info)
-	return [series_name, anime_info[3], html_info(anime_info, "MAL")]
+	store_new_name(orginal, anime_info[0], anime_info[4], 0)
+	return html_info(anime_info[0], anime_info, "MAL")
 
 def AnimeNewsNetword_search(query):
 	if query == "":
@@ -261,8 +379,8 @@ def AnimeNewsNetword_search(query):
 
 	series_name = re.sub(r'\(TV[^)]*\)', '', series_name).strip()
 	anime_info = [title, image, synopsis, show_type, total_eps, anime_id, "ANN"]
-	store_new_name(orginal, anime_info)
-	return [series_name, anime_info[3], html_info(anime_info, "ANN")]
+	store_new_name(orginal, anime_info[0], anime_info[4], 0)
+	return html_info(anime_info[0], anime_info, "ANN")
 
 def get_series_info(name):
 	print "Searching info for %s" % name
